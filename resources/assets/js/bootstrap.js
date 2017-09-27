2f0cce6d8e5249f9916d6590d159139e5bc72f59
zhaojipeng <zhaojipeng@haodai.com>
2017-09-27 17:41:23 +0800

    支持iView

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,18 +1,3 @@
-
-window._ = require('lodash');
-
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
-} catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
@@ -36,6 +21,10 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+
+window.iview = require('iview');
+require('iview/dist/styles/iview.css');
+
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
